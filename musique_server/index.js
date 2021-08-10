c7794c3ae7faf0c7030f506a87fbda7a52a7d03f
Andraitis Vilius <vilius.andraitis@mif.stud.vu.lt>
2021-08-10 15:56:57 +0300

    initial

---
+++ musique_server/index.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const errorHandler = require("./handlers/error");
+const authRoutes = require("./routes/auth");
+const messagesRoutes = require("./routes/messages");
+const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
+
+const PORT = 8081;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use(
+  "/api/users/:id/messages",
+  loginRequired,
+  ensureCorrectUser,
+  messagesRoutes
+);
+
+// all my routes here - they will come later
+
+app.get("/api/messages", loginRequired, async function (req, res, next) {
+  try {
+    let messages = await db.Message.find()
+      .sort({ createdAt: "desc" })
+      .populate("user", {
+        username: true,
+        profileImageUrl: true,
+      });
+    return res.status(200).json(messages);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.use(function (res, res, next) {
+  let err = new Error("Not found");
+  err.status = 404;
+  next(err);
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+});
